920baf4684b5e7ab5392318a496b5ab40a5049cc
Flabio Hinestroza Sanclemente <flabiohs@gmail.com>
2024-08-23 07:17:17 -0500

    package var_db

---
+++ constantdb.go
@@ -0,0 +1,12 @@
+package safeutils
+
+const DB_DIFF_ID string = "id <>?"
+const DB_EQUAL_ID string = "id=?"
+const DB_EQUAL_NAME string = "name =?"
+const DB_ORDER_DESC string = "id desc"
+const DB_EQUAL_EMAIL_ID string = "email=?"
+const DB_EQUAL_USER_ID string = "user_id =?"
+const DB_EQUAL_CITY_ID string = "city_id =?"
+const DB_LIKE_NAME string = "name LIKE?"
+const ID_NO_EXIST string = "The id not exists"
+const ACTIVE string = "active"
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/flabio/safe_var_db
+
+go 1.22.3
